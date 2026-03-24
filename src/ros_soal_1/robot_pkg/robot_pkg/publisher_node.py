d562ab329e783532253a32e2c333bfe63335230e
aria1811 <dragonflies96969@gmail.com>
2026-03-24 18:00:07 +0700

    Add robot_pkg ROS2 project

---
+++ src/ros_soal_1/robot_pkg/robot_pkg/publisher_node.py
@@ -0,0 +1,22 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class PublisherNode(Node):
+    def __init__(self):
+        super().__init__('publisher_node')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        self.timer = self.create_timer(1.0, self.publish_message)
+
+    def publish_message(self):
+        msg = String()
+        msg.data = 'Halo, ini dari Publisher!'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Mengirim: {msg.data}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PublisherNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ src/ros_soal_1/robot_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aria1811',
+    maintainer_email='aria1811@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'publisher = robot_pkg.publisher_node:main',
+            'subscriber = robot_pkg.subscriber_node:main',
+        ],
+    },
+)
