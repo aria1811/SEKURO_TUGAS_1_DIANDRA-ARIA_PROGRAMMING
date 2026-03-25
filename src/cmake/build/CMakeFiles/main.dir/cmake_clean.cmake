c9af2eb7f8e761c60de8091105444e17b52ea656
aria1811 <dragonflies96969@gmail.com>
2026-03-25 18:43:56 +0700

    CMake

---
+++ src/cmake/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/robot.cpp.o"
+  "CMakeFiles/main.dir/src/robot.cpp.o.d"
+  "CMakeFiles/main.dir/src/robotair.cpp.o"
+  "CMakeFiles/main.dir/src/robotair.cpp.o.d"
+  "bin/main"
+  "bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
